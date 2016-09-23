f357006b00388596287faa720810855752e5d0f3
Ryan Bell <rjb1928@rit.edu>
2016-09-22 23:27:47 -0400

    changed small jump times

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ function controlVideo(command){
     var $video = $('video')[0];
     switch(command){
         case "medium_back_jump":
-            $video.currentTime -= 5;
+            $video.currentTime -= 15;
             return "successful " + command;
         case "medium_forward_jump":
-            $video.currentTime += 5;
+            $video.currentTime += 15;
             return "successful " + command;
         case "long_back_jump":
             $video.currentTime -= 60;
